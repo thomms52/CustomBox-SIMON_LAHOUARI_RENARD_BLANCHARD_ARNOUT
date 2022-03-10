8e25bc768581e5002716543b30eb31f227816804
LordCourgette <fabrice.arnout@gmail.com>
2022-03-10 15:29:54 +0100

    ajouts de bootstrap

--- src/view/VueRender.php
+++ src/view/VueRender.php
@@ -29,7 +29,11 @@ class VueRender
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
                 <title>Home - Brand</title>
-               
+                <link rel="stylesheet" href="{$this->container->router->pathFor("accueil")}assets/bootstrap/css/bootstrap.min.css">
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+                <link rel="stylesheet" href="{$this->container->router->pathFor("accueil")}assets/fonts/simple-line-icons.min.css">
+                <link rel="stylesheet" href="{$this->container->router->pathFor("accueil")}assets/css/vanilla-zoom.min.css">
+                <link rel="stylesheet" href="{$this->container->router->pathFor("accueil")}css/style.css">
 
             </head>
             <body>
