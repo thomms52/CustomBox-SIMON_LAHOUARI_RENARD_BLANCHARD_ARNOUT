7c56977893d5c47163c69478c0440e04b356526f
LoicBlanchard7 <90914694+LoicBlanchard7@users.noreply.github.com>
2022-03-10 15:00:40 +0100

    Ajout de l'interface commande

--- src/view/VueCommande.php
+++ src/view/VueCommande.php
@@ -10,19 +10,60 @@ class VueCommande {
 	
 	private function CreationCommande(): String {
         $content = '';
-        $content.='<h1>Commandes</h1>';
+        $content.='
+		<p id="titre">Commandes</p>
+		
+		<div class="cartonGauche">
+			<div> 
+				<img src="../images/box.svg">
+				
+				<button class="favorite styled"
+					type="button">
+						Grand
+				</button>
+				
+				<button class="favorite styled"
+					type="button">
+						Moyen
+				</button>
+				
+				<button class="favorite styled"
+					type="button">
+						Petit
+				</button>
+							
+				<div>
+					<input type="color" id="Couleur" name="Couleur"
+					value="#ffffff">
+					<label for="head">Couleur</label>
+				</div>
+				
+				<form method="post" action="traitement.php">
+					<p>
+						<label for="ameliorer">Quel est la description de la boite ?</label><br />
+						<textarea name="ameliorer" id="ameliorer"></textarea>
+					</p>
+				</form>
+			</div>
+			
+		</div>
+		<div class="cartonDroite">
+			<p>Carton mais à droite</p>
+		</div>
+		';
         return $content;
     }
 
 	
 	public function render() {
         $content = $this->CreationCommande();
+		
+		
         
         $html = <<<END
             <!DOCTYPE html>
             <html>
-            <body><head>
-
+            <body><head><link rel="stylesheet" href="../css/StyleCommande.css">
             </head>
                 $content
             </body>
