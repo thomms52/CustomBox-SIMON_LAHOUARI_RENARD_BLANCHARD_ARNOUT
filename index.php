6006ee707eeb0cd2da6db63898a7d67505008523
dranerine <74104215+dranerine@users.noreply.github.com>
2022-03-10 10:50:11 +0100

    ajout controleur/ vue d'un produit

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php
 declare(strict_types=1);
 
+# Chargement de l'autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+use custombox\controleur\ControlerGestionProduit;
+use custombox\view\VueGestionProduit;
+
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Illuminate\Database\Capsule\Manager as DB;
+use Slim\App;
+use Slim\Container;
+
+# Installation de la configuration erreur de Slim
+$config = ['settings' => ['displayErrorDetails' => true, 'dbconf' => __DIR__.'/src/config/dbconfig.ini']];
+
+# Connection a la base de donnees MYSQL
+# Chargement du module Eloquent
+$db = new DB();
+$db->addConnection(parse_ini_file(__DIR__.'/src/config/dbconfig.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$container = new Container($config);
+$app = new App($container);
+
+
+
+########    LES ROUTES  #######
+
+//Fonction 1, liste des produits
+
+$app->get('/listeProduit/[/]', function (Request $rq, Response $rs, array $args) use ($container): Response {
+    $controleur = new ControlerGestionProduit($container);
+    return $controleur->AffichageListe($rq, $rs, $args, false);
+})->setName('afficherListe');
+
